63b1b142ee5e3e26d3746b58401f9aa10c9d8802
noah-berman <noahberm@gmail.com>
2018-08-21 16:41:26 -0400

    updated navbar

--- src/components/NavHeader.js
+++ src/components/NavHeader.js
@@ -28,8 +28,8 @@ const NavHeader = (props) => {
 				<h1 className="App-title">Meme Wars</h1>
 				<span className="tagline">One Meme to Rule Them All</span>
 				<span>
-							<NavLink onClick={props.handleClick} activeClassName="active" to="/" style={linkStyle}> Home </NavLink>
-							<NavLink onClick={props.handleClick} activeClassName="active" to="/profile" style={linkStyle} > Profile </NavLink>
+							<NavLink onClick={props.handleClick} activeClassName="active" to="/" style={linkStyle}> View All Memes </NavLink>
+							{/* <NavLink onClick={props.handleClick} activeClassName="active" to="/profile" style={linkStyle} > Profile </NavLink> */}
 							<NavLink onClick={props.handleClick} activeClassName="active" to="/create" style={linkStyle}> Make A Meme </NavLink>
 				</span>
 			</header>
