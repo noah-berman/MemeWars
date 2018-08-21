2bfd38c5771146f31307658c05c22afec344595a
DevinPierce <devinmpierce@gmail.com>
2018-08-21 09:47:49 -0400

    added meme creation buttons

--- src/components/SearchImagesResults.js
+++ src/components/SearchImagesResults.js
@@ -2,40 +2,71 @@
 
 import React from 'react'
 
+import Lightbox from 'react-image-lightbox';
+import 'react-image-lightbox/style.css';
+
 import SearchResultImage from './SearchResultImage'
 
-export default function SearchImagesResults (props) {
+export default class SearchImagesResults extends React.Component {
 
-  function renderImages(){
-    if (props.imgurResults.length > 0){
+  state = {
+    photoIndex: 0,
+    isOpen: false
+  }
+
+  openLightbox = (index) => {
+    this.setState({
+      photoIndex: index,
+      isOpen: true
+    })
+  }
+
+  renderImages(){
+    if (this.props.imgurResults.length > 0){
       return (
         <React.Fragment>
           <div id='prevnext-button-wrapper'>
-            <span className="prevnext-button"  onClick={props.imgurPrev}>
+            <span className="prevnext-button"  onClick={this.props.imgurPrev}>
               Previous
             </span>
-            <span className="prevnext-button" onClick={props.imgurNext}>
+            <span className="prevnext-button" onClick={this.props.imgurNext}>
               Next
             </span>
           </div>
           <div id="results-images">
-            {imgurRender()}
+            {this.imgurRender()}
+          </div>
+          <div id='prevnext-button-wrapper'>
+            <span className="prevnext-button"  onClick={this.props.imgurPrev}>
+              Previous
+            </span>
+            <span className="prevnext-button" onClick={this.props.imgurNext}>
+              Next
+            </span>
           </div>
         </React.Fragment>
       )
-    } else if (props.giphyResults.length > 0) {
+    } else if (this.props.giphyResults.length > 0) {
       return (
         <React.Fragment>
           <div id='prevnext-button-wrapper'>
-            <span className="prevnext-button" onClick={props.giphyPrev}>
+            <span className="prevnext-button" onClick={this.props.giphyPrev}>
               Previous
             </span>
-            <span className="prevnext-button" onClick={props.giphyNext}>
+            <span className="prevnext-button" onClick={this.props.giphyNext}>
               Next
             </span>
           </div>
           <div id="results-images">
-            {giphyRender()}
+            {this.giphyRender()}
+          </div>
+          <div id='prevnext-button-wrapper'>
+            <span className="prevnext-button" onClick={this.props.giphyPrev}>
+              Previous
+            </span>
+            <span className="prevnext-button" onClick={this.props.giphyNext}>
+              Next
+            </span>
           </div>
         </React.Fragment>
       )
@@ -44,58 +75,66 @@ export default function SearchImagesResults (props) {
    }
   }
 
-  const imgurRender = () => {
-    const results = [...props.imgurResults]
-    return results.map(imageObj => {
-      return <SearchResultImage key={imageObj.id} api='imgur' imageObj={imageObj} />
+  imgurRender = () => {
+    const results = [...this.props.imgurResults]
+    return results.map((imageObj, index) => {
+      return <SearchResultImage
+        key={imageObj.id}
+        api='imgur'
+        imageObj={imageObj}
+        openLightbox={this.openLightbox}
+        index={index}
+      />
     })
   }
-  // const imgurRender = () => {
-  //
-  //   let firstResources = [...props.imgurResults]
-  //   // firstResources.length = 10
-  //
-  //   let imgTags = firstResources.map((imageObj, i) => {
-  //
-  //     // console.log(i, imageObj, imageObj.images)
-  //
-  //     if (!imageObj.is_album) {
-  //       if (imageObj.type === "image/jpeg"){
-  //         return <img key={imageObj.id} src={imageObj.link} alt={imageObj.title} className="search-result-image"/>
-  //       } else if (imageObj.type === "image/gif") {
-  //         return <img key={imageObj.id} src={imageObj.link} alt={imageObj.title} className="search-result-image" />
-  //       }
-  //     } else if (imageObj.is_album){
-  //       return imageObj.images.map(image => {
-  //         if (image.type === "image/jpeg"){
-  //           return <img key={image.id} src={image.link} alt={imageObj.title} className="search-result-image" />
-  //         } else if (image.type === "image/gif"){
-  //           return <img key={image.id} src={image.link} alt={imageObj.title} className="search-result-image" />
-  //         }
-  //       })
-  //     }
-  //   })
-  //   console.log(imgTags)
-  //   return imgTags
-  // }
 
-  const giphyRender = () => {
-    const results = [...props.giphyResults]
-    return results.map(imageObj => {
-      return <SearchResultImage key={imageObj.id} api='giphy' imageObj={imageObj} />
+  giphyRender = () => {
+    const results = [...this.props.giphyResults]
+    return results.map((imageObj, index) => {
+      return <SearchResultImage
+        key={imageObj.id}
+        api='giphy'
+        imageObj={imageObj}
+        openLightbox={this.openLightbox}
+        index={index}
+      />
     })
   }
-  // const giphyRender = () => {
-  //   const results = [...props.giphyResults]
-  //   return results.map(imageObj => {
-  //     return <img key={imageObj.id} src={imageObj.images.downsized.url} alt={imageObj.title} className="search-result-image" />
-  //   })
-  // }
 
-  return (
-    <div id="results-wrapper">
-      {renderImages()}
-    </div>
-  )
+  render (){
+    const { photoIndex, isOpen } = this.state;
+    let images;
+    if (this.props.imgurResults.length > 0){
+      images = this.props.imgurResults.map(imageObj=>imageObj.link)
+    } else if (this.props.giphyResults.length > 0){
+      images = this.props.giphyResults.map(imageObj=>imageObj.images.original.url)
+    }
 
+    return (
+      <div id="results-wrapper">
+        {this.renderImages()}
+
+        {isOpen && (
+          <Lightbox
+            mainSrc={images[photoIndex]}
+            nextSrc={images[(photoIndex + 1) % images.length]}
+            prevSrc={images[(photoIndex + images.length - 1) % images.length]}
+            onCloseRequest={() => this.setState({ isOpen: false })}
+            onMovePrevRequest={() =>
+              this.setState({
+                photoIndex: (photoIndex + images.length - 1) % images.length
+              })}
+            onMoveNextRequest={() =>
+              this.setState({
+                photoIndex: (photoIndex + 1) % images.length
+              })}
+            toolbarButtons={[<span
+              className='memeify-button-toolbar'
+              // onClick={}
+              >Memeify</span>]}
+          />
+        )}
+      </div>
+    )
+  }
 }
